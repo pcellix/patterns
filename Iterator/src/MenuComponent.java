5a4281b189d2c9acd405d7aff6a8896e1174a495
Wojtek warsaw <pcellix@gmail.com>
2013-08-26 22:55:15 +0200

    Added abstract MenuComponent Class

---
+++ Iterator/src/MenuComponent.java
@@ -0,0 +1,22 @@
+
+public abstract class MenuComponent {
+	
+	public void add(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	public void remove(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	public void getChild(int i) {
+		throw new UnsupportedOperationException();
+	}	
+	public void getName() {
+		throw new UnsupportedOperationException();
+	}	
+	public String getDescription() {
+		throw new UnsupportedOperationException();
+	}	
+	public void print() {
+		throw new UnsupportedOperationException();
+	}
+}
